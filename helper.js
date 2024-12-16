b45c403e0796bd47158ccc78295722f0ea51a420
Clarkd341 <rotondo.d@outlook.com>
2024-12-16 12:15:06 +0100

    Helper.js

---
+++ helper.js
@@ -0,0 +1,30 @@
+
+// Méthode 3 plus court
+exports.success = (message,data) => {
+    return {message, data }
+  }
+
+
+// Méthode 2 
+/*
+  exports.success = (message,data) => {
+    return {
+
+       message : message,
+       data : data
+
+    }
+  }
+*/
+
+  // Methode 1
+/*const success = (message,data) => {
+
+    return {
+
+       message : message,
+       data : data
+
+    }
+  }
+*/
