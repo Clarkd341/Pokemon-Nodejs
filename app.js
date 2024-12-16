35fb3418099a3c7368241da3b00c3d95c1361905
Clarkd341 <rotondo.d@outlook.com>
2024-12-16 11:02:29 +0100

    Stop

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pokemons = require('./mock-pokemon');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello again, express 2!');
+});
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+res.send(`Vous avez demander le pokémon ${pokemon.name}`)
+
+})
+
+
+app.get('/api/pokemons', (req, res) => {
+ res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`)
+})
+
+
+// lancer serveur
+app.listen(port, () => {
+  console.log(`Notre application node est démarrée sur : http://localhost:${port}`);
+});
